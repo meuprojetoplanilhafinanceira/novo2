56005265df7db803bfb6246a21b654149000caa8
jcrezbaptista <>
2021-01-24 22:51:15 -0300

    segundo

---
+++ web/script/texto1.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.write(<p class="text1">"O grande desafio dos brasileiros é a organização de suas vidas financeiras,
+        por isto é muito importante o controle de suas finanças. "</p>);
+
